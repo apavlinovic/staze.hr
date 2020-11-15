3d03864284853e6a029bbbae7f0f3ce86158e5ac
Antonio Pavlinovic <antonio@dump.hr>
2020-11-15 12:24:59 +0100

    Registration page design and footer updates

--- web/src/common/app/header/Header.tsx
+++ web/src/common/app/header/Header.tsx
@@ -4,21 +4,25 @@ import { NavLink } from 'react-router-dom';
 import './Header.scss';
 import Logo from './../../core/logo/Logo';
 import MainMenu from './../main-menu/MainMenu';
+import UserMenu from './../user-menu/UserMenu';
 import OmniSearch from './../../ui/omni-search/OmniSearch';
 
 function Header() {
     return (
         <header className="common-header">
-            <div className="item">
+            <div className="item logo">
                 <NavLink to="/" className="homepage-link">
                     <Logo variant="graphic-only" />
                 </NavLink>
             </div>
-            <div className="item">
+            <div className="item menu">
                 <MainMenu />
             </div>
-            <div className="item">
+            <div className="item search">
                 <OmniSearch />
+            </div>
+            <div className="item user-menu">
+                <UserMenu />
             </div>
         </header>
     );
--- web/src/pages/auth/Register.tsx
+++ web/src/pages/auth/Register.tsx
@@ -1,8 +1,13 @@
 import React, { useContext } from 'react';
 import { withTranslation, WithTranslation } from 'react-i18next';
 import { FieldErrors, useForm } from 'react-hook-form';
+
 import { AuthContext } from '../../common/core/auth-context/AuthContext';
 import Input from '../../common/core/forms/Input';
+import Card from '../../common/ui/cards/card/Card';
+import { ReactComponent as RegisterIllustration } from './RegisterIllustration.svg';
+
+import './Register.scss';
 
 interface RegistrationFormFields {
     email: string;
@@ -24,39 +29,60 @@ function Register(props: WithTranslation) {
         console.error(data);
 
     return (
-        <main>
-            <h1>{t('verb.register')}</h1>
-            <form onSubmit={handleSubmit(onSubmit, onError)}>
-                <Input
-                    name="email"
-                    type="email"
-                    required={true}
-                    label="noun.email"
-                    errors={errors}
-                    register={register}
-                />
+        <div className="page-register container">
+            <Card>
+                <div className="grid">
+                    <div className="grid-item large-span-12 small-span-12 small-align-content-center">
+                        <h1 className="small-text-align-center">
+                            {t('strings.register_page_title')}
+                        </h1>
+                    </div>
+                    <div className="grid-item large-span-6 small-span-12 small-align-content-center">
+                        <p className="small-text-align-center">
+                            {t('strings.register_page_description')}
+                        </p>
+                        <br />
+                        <RegisterIllustration />
+                    </div>
+                    <div className="grid-item large-span-6 small-span-12">
+                        <form onSubmit={handleSubmit(onSubmit, onError)}>
+                            <Input
+                                name="email"
+                                type="email"
+                                required={true}
+                                label="noun.email"
+                                errors={errors}
+                                register={register}
+                            />
 
-                <Input
-                    name="password"
-                    type="password"
-                    required={true}
-                    label="noun.password"
-                    errors={errors}
-                    register={register}
-                />
+                            <Input
+                                name="password"
+                                type="password"
+                                required={true}
+                                label="noun.password"
+                                errors={errors}
+                                register={register}
+                            />
 
-                <Input
-                    name="username"
-                    label="noun.username"
-                    maxLength={100}
-                    errors={errors}
-                    pattern={/[A-Za-z0-9^\s]+/}
-                    register={register}
-                />
+                            <Input
+                                name="username"
+                                label="noun.username"
+                                maxLength={100}
+                                errors={errors}
+                                pattern={/[A-Za-z0-9^\s]+/}
+                                register={register}
+                            />
 
-                <input type="submit" value={t('verb.submit') as string} />
-            </form>
-        </main>
+                            <input
+                                type="submit"
+                                className="__submit"
+                                value={t('verb.submit') as string}
+                            />
+                        </form>
+                    </div>
+                </div>
+            </Card>
+        </div>
     );
 }
 
