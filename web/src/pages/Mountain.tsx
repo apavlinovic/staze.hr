11c669da2ea758c5a8373adcb81cd2accb77330a
Ivan Pirija <pinjamar@gmail.com>
2021-01-14 13:06:24 +0100

    trails list added (#21)
    * trails list added to mountains
    
    * edited trails list page
    
    * trail list edited
    
    * solved requested changes
    
    * edited mountains title

--- web/src/pages/Mountain.tsx
+++ web/src/pages/Mountain.tsx
@@ -5,7 +5,14 @@ import { withTranslation, WithTranslation } from 'react-i18next';
 import Loading from '../common/core/loading/Loading';
 import Error from '../common/core/error/Error';
 import { Query, QueryAreaArgs } from '../types';
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
+import './Mountain.scss';
+import Distance from '../common/ui/field-renderers/distance/Distance';
+import Duration from '../common/ui/field-renderers/duration/Duration';
+import HeightDifference from '../common/ui/field-renderers/height-difference/HeightDifference';
+
+import { ReactComponent as MountainSearchIcon } from '../common/ui/omni-search/MountainSearchIcon.svg';
+import Coordinates from '../common/ui/field-renderers/coordinates/Coordinates';
 
 const AREA_QUERY = gql`
     query getAreaInformation($areaSlug: String!) {
@@ -13,6 +20,21 @@ const AREA_QUERY = gql`
             id
             name
             slug
+        }
+        trails(mountain: $areaSlug) {
+            items {
+                id
+                name
+                duration
+                distance
+                slug
+                description
+                heightDifference
+                startLocation
+                startLocationCoords {
+                    coordinates
+                }
+            }
         }
     }
 `;
@@ -42,9 +64,89 @@ function Mountain(props: WithTranslation) {
     }
 
     return (
-        <main>
-            <h1>{data?.area?.name}</h1>
-        </main>
+        <div className="mountains--page">
+            <div
+                className="trails-header"
+                style={{
+                    backgroundImage: `url(/mountains/${data?.area?.slug}.jpg)`,
+                }}
+            >
+                <h1 className="trail-name">
+                    <MountainSearchIcon className="header-icon" />
+                    <span className="trail-text">{data?.area?.name}</span>
+                </h1>
+            </div>
+            <div className="content-wrapper">
+                <div className="trails-list">
+                    <h1 className="trail-title">
+                        {data?.area?.name} — {data?.trails?.items.length}{' '}
+                        {t('noun.trails')}{' '}
+                    </h1>
+                    <div className="filters">
+                        <button>{t('strings.all_trails')}</button>
+                        <button>{t('strings.short_trails')}</button>
+                        <button>
+                            {t('strings.duration_over', {
+                                hours: 1,
+                            })}
+                        </button>
+                        <button>
+                            {t('strings.duration_over', {
+                                hours: 3,
+                            })}
+                        </button>
+                    </div>
+                    <ul className="trails">
+                        {data?.trails?.items.map((trail, index) => (
+                            <div className="trail" key={`mountain-${index}`}>
+                                <li>
+                                    <Link to={`/trail/${trail.slug}`}>
+                                        {trail.name}
+                                    </Link>
+                                </li>
+                                <div className="trails-about">
+                                    <li className="trail-distance">
+                                        <Distance distance={trail.distance} />{' '}
+                                        <br />
+                                        <span className="trail-specs">
+                                            {t('noun.distance')}
+                                        </span>
+                                    </li>
+                                    <li className="trail-duration">
+                                        <Duration duration={trail.duration} />{' '}
+                                        <br />
+                                        <span className="trail-specs">
+                                            {t('noun.duration')}
+                                        </span>
+                                    </li>
+                                    <li className="trail-height-difference">
+                                        <HeightDifference
+                                            difference={trail.heightDifference}
+                                        />{' '}
+                                        <br />
+                                        <span className="trail-specs">
+                                            {t('noun.height_difference')}
+                                        </span>
+                                    </li>
+                                    <li className="trail-start">
+                                        <span className="trail-specs">
+                                            {t('noun.start_location')}
+                                        </span>{' '}
+                                        <br />
+                                        {trail.startLocation} (
+                                        <Coordinates
+                                            geopoint={trail.startLocationCoords}
+                                        />
+                                        )
+                                    </li>
+                                </div>
+                            </div>
+                        ))}
+                    </ul>
+                </div>
+                <div className="map">Neka slika</div>
+            </div>
+        </div>
     );
 }
 
