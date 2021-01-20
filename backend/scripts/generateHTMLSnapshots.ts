6d95012e588bffa8ad29219ade078f3f55438195
Ivan Pirija <pinjamar@gmail.com>
2021-01-20 08:09:19 +0100

    Generate html snapshot (#22)
    * trails list added to mountains
    
    * added puppeteer scripts
    
    * pagesize changed

--- backend/scripts/generateHTMLSnapshots.ts
+++ backend/scripts/generateHTMLSnapshots.ts
@@ -1,25 +1,83 @@
 require('dotenv').config();
 
 import { DATABASE_CONNECTION } from '../src/database/db-connection';
-// import { TrailResolver } from '../src/modules/trails/trail.resolver';
+import { TrailResolver } from '../src/modules/trails/trail.resolver';
+import { AreaResolver } from '../src/modules/areas/area.resolver';
+import { launch } from 'puppeteer';
+import { writeFileSync } from 'fs';
+import { join } from 'path';
+
+const SERVER_ROOT = 'http://localhost:3000';
 
 (async function () {
     DATABASE_CONNECTION.then(async () => {
-        // const trailResolver = new TrailResolver();
-        // const trails = await trailResolver.getTrails({
-        //     pageSize: 3,
-        //     offset: 0,
-        //     orderBy: null,
-        //     maintainer: null,
-        //     nearby: null,
-        //     mountain: null,
-        //     distance: null,
-        //     duration: null,
-        // });
+        const trailResolver = new TrailResolver();
+        const trails = await trailResolver.getTrails({
+            pageSize: 2000,
+            offset: 0,
+            orderBy: null,
+            maintainer: null,
+            nearby: null,
+            mountain: null,
+            distance: null,
+            duration: null,
+        });
 
-        // for (const trail of trails.items) {
-        //     // #1 KORISTIT PUPETEER ZA DOHVATIT HTML OD OVOG TRAILA
-        //     // #2 SPREMIT HTML U ../artifacts (1004-zapadak-crni-vrh.html)
-        // }
+        const browser = await launch({
+            headless: true,
+            defaultViewport: null,
+        });
+
+        const page = await browser.newPage();
+
+        for (const trail of trails.items) {
+            await page.goto(`${SERVER_ROOT}/trail/${trail.slug}`, {
+                waitUntil: 'networkidle0',
+            });
+
+            const htmlContent = await page.content();
+            const filename = `trail-${trail.slug}.html`;
+
+            writeFileSync(
+                join(__dirname, `../../artifacts/${filename}`),
+                htmlContent,
+            );
+        }
+
+        await browser.close();
+    });
+})();
+
+(async function () {
+    DATABASE_CONNECTION.then(async () => {
+        const areaResolver = new AreaResolver();
+        const areas = await areaResolver.getAreas({
+            pageSize: 1000,
+            offset: 0,
+            orderBy: null,
+        });
+
+        const browser = await launch({
+            headless: true,
+            defaultViewport: null,
+        });
+
+        const page = await browser.newPage();
+
+        for (const area of areas.items) {
+            await page.goto(`${SERVER_ROOT}/area/${area.slug}`, {
+                waitUntil: 'networkidle2',
+            });
+
+            const htmlContent = await page.content();
+            const filename = `area-${area.slug}.html`;
+
+            writeFileSync(
+                join(__dirname, `../../artifacts/${filename}`),
+                htmlContent,
+            );
+        }
+
+        await browser.close();
     });
 })();
